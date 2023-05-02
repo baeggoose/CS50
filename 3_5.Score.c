b411f25c28e99285f3b280f9ddee570076aa7c41
baeggoose <bsj154@gmail.com>
2023-05-02 18:48:02 +0900

    3_5.Array(2) average

--- 3_5.Score.c
+++ 3_5.Score.c
@@ -1,6 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
+float average(int length, int array[]);
 int main(void)
 {
   int n = get_int("Number of scores: ");
@@ -9,8 +10,18 @@ int main(void)
   
   for (int i =0; i < n; i++)
   {
-    scores[i] = get_int("Score: ");
+    scores[i] = get_int("Score %i: ",i+1);
   }
 
-  printf("Average: TODO\n");
+  printf("Average: %.1f\n", average(n,scores));
+}
+
+float average(int length, int array[])
+{
+  int sum = 0;
+  for(int i = 0; i < length; i++)
+  {
+    sum += array[i];
+  }
+  return (float) sum / (float) length;
 }
