fd250fa7937d335ea62ca2ed390b7d70dd798d52
baeggoose <bsj154@gmail.com>
2023-05-02 18:33:06 +0900

    3_5.Array(2) 배열의 동적 선언

--- 3_5.Score.c
+++ 3_5.Score.c
@@ -1,14 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
 
-const int N = 3;
-
 int main(void)
 {
-  int scores[N];
-  scores[0] = 72;
-  scores[1] = 73;
-  scores[2] = 33;
+  int n = get_int("Number of scores: ");
 
-  printf("Average: %i\n", (scores[0] + scores[1] + scores[2]) / 3);
+  int scores[n];
+  
+  for (int i =0; i < n; i++)
+  {
+    scores[i] = get_int("Score: ");
+  }
+
+  printf("Average: TODO\n");
 }
