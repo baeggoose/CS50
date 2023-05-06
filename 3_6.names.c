60293337d4c98938476f37229440d646028aada2
baeggoose <bsj154@gmail.com>
2023-05-06 14:41:46 +0900

    3_6.문자열과 배열

---
+++ 3_6.names.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+  string names[4];
+  names[0]= "EMMA";
+  names[1]= "RODRIGO";
+  names[2]= "BRIAN";
+  names[3]= "DAVID";
+
+  printf("%s\n", names[0]);
+}
